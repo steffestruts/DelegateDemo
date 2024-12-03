b3652174ec5a32f8cb31e065196a20a1abc23826
Stefan Sjöberg <bird.nerd@outlook.com>
2024-12-03 13:47:33 +0100

    Add project files.

---
+++ DelegateDemo/Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+
+        public void Work()
+        {
+            var mediaInventory = new MediaInventory();
+            var recordPlayer = new RecordPlayer();
+            var cassettePlayer = new CassettePlayer();
+
+            MediaInventory.TestMedia testRecordDelegate =
+                new MediaInventory.TestMedia(recordPlayer.PlayRecord);
+            MediaInventory.TestMedia testCasetteDelegate =
+                new MediaInventory.TestMedia(cassettePlayer.TestCasette);
+
+            mediaInventory.TestResult(testRecordDelegate);
+            mediaInventory.TestResult(testCasetteDelegate);
+        }
+    }
+}
